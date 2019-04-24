27dadeeb3d29e4f198cd30bad1b46aa202cd82d6
Mystic67 <webmaster@passion-geek.com>
2019-04-24 14:16:15 +0200

    init config file

---
+++ config/open_ff_settings.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+# -*-coding: utf-8-*-
+
+#Open Food Facts settings:
+url= "https://fr.openfoodfacts.org/cgi/search.pl?"
+
+default_search_params= {
+"tagtype_0" : "categories",
+"countries" : "France",
+"tag_contains_0" : "contains",
+"search_simple" : "1",
+"action" : "process",
+"page_size" : "1",
+"page" : "1",
+"json" : "1"
+}
+
+#User choise
+categories=["Viande","Plats préparés","Boissons","Petit_déjeunés","Fromages"]
+
+# Desired food information:
+fr_food_informations = { "Non du produit" : "product_name", "Nom générique" : "generic_name",\
+     "Marques" : "brands", "Catégories" : "categories", "Note nutritionnelle (A à E)" : "nutrition_grades",\
+     "Groupe Nova (1 à 4)" : "nova_groups", "Allergènes" : "allergens", 
+          "Lien Open Food Facts" : "url" }
