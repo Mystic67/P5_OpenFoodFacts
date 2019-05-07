d9a17e4f2c152698189abbb322941fb27cb5d264
Mystic67 <webmaster@passion-geek.com>
2019-05-07 13:59:00 +0200

    Updates tables création queries

--- config/db_settings.py
+++ config/db_settings.py
@@ -17,15 +17,15 @@ TABLES = {}
 TABLES['store'] = (
     "CREATE TABLE IF NOT EXISTS stores ( \
         id INT AUTO_INCREMENT NOT NULL, \
-        store VARCHAR(50) NOT NULL, \
+        store VARCHAR(50) UNIQUE NOT NULL, \
         PRIMARY KEY (id) ) \
-    ENGINE=InnoDB"
+    ENGINE=InnoDB;"
     )
 
 TABLES['categories'] = (
     "CREATE TABLE IF NOT EXISTS categories ( \
         id INT AUTO_INCREMENT NOT NULL, \
-        categorie VARCHAR(50) NOT NULL, \
+        category VARCHAR(50) UNIQUE NOT NULL, \
         PRIMARY KEY (id) \
         ) \
     ENGINE=InnoDB;"
@@ -34,12 +34,12 @@ TABLES['categories'] = (
 TABLES['products'] = (
     "CREATE TABLE IF NOT EXISTS products ( \
         id BIGINT AUTO_INCREMENT NOT NULL, \
-        product_name VARCHAR(50) NOT NULL, \
-        generic_name VARCHAR(50) NOT NULL, \
+        product_name VARCHAR(200) UNIQUE NOT NULL, \
+        generic_name VARCHAR(300) NOT NULL, \
         brands VARCHAR(50) NOT NULL, \
-        url VARCHAR(255) NOT NULL, \
         nutrition_grades CHAR(1) NOT NULL, \
-        allergens VARCHAR(255) NOT NULL, \
+        allergens VARCHAR(255) NULL, \
+        url VARCHAR(255) NOT NULL, \
         PRIMARY KEY (id) \
         ) \
     ENGINE=InnoDB;"
@@ -48,9 +48,11 @@ TABLES['products'] = (
 TABLES['substitutes'] = (
     "CREATE TABLE IF NOT EXISTS substitutes ( \
         id BIGINT AUTO_INCREMENT NOT NULL, \
-        Products_id BIGINT NOT NULL, \
-        alternative BIGINT NOT NULL, \
+        products_id BIGINT NOT NULL, \
+        alternative_id BIGINT NOT NULL, \
         PRIMARY KEY (id), \
+        UNIQUE KEY products_id (products_id), \
+        UNIQUE KEY alternative_id (alternative_id), \
         CONSTRAINT products_substitutes_fk \
         FOREIGN KEY (Products_id) \
         REFERENCES products (id) \
@@ -61,9 +63,10 @@ TABLES['substitutes'] = (
 
 TABLES['products_categories'] = (
     "CREATE TABLE IF NOT EXISTS products_categories ( \
+        id BIGINT AUTO_INCREMENT NOT NULL, \
         product_id BIGINT NOT NULL, \
         category_id INT NOT NULL, \
-        PRIMARY KEY (product_id), \
+        PRIMARY KEY (id), \
         CONSTRAINT categories_products_categories_fk \
         FOREIGN KEY (category_id) \
         REFERENCES categories (id), \
@@ -76,9 +79,10 @@ TABLES['products_categories'] = (
 
 TABLES['products_stores'] = (
     "CREATE TABLE IF NOT EXISTS products_stores ( \
+        id BIGINT AUTO_INCREMENT NOT NULL, \
         product_id BIGINT NOT NULL, \
         store_id INT NOT NULL, \
-        PRIMARY KEY (product_id), \
+        PRIMARY KEY (id), \
         CONSTRAINT stores_products_stores_fk \
         FOREIGN KEY (store_id) \
         REFERENCES stores (id), \
