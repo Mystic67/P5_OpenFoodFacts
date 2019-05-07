b0b4efceebdce8c8431f84ae44ac8706d1e60fc0
Mystic67 <webmaster@passion-geek.com>
2019-05-07 14:11:02 +0200

    Update table creation and add method

--- db/mysql_db.py
+++ db/mysql_db.py
@@ -3,10 +3,14 @@
 
 import mysql.connector
 from mysql.connector import errorcode
+import config.open_ff_settings as constants
 import config.db_settings as db_constants
+import config.queries_settings as db_data_constants
+import config.display_settings as display_constants
+
 
 class Mysql_db:
-
+    '''This class connect user to database, create the database and the tables and put the OpenFoodFacts data into the tables'''
     def __init__(self):
         self.connect = self.connection()
         self.cursor = self.connect.cursor()
@@ -20,29 +24,65 @@ class Mysql_db:
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
                 print("Non d'utilisateur ou mot de passe d'accès à la DB erroné")
-    
+
     def create_database(self):
-        try: 
+        try:
             self.cursor.execute("USE {}".format(db_constants.DB_NAME))
-            print("Connecté à la base de données {}".format(db_constants.DB_NAME))
+            print(display_constants.colours["black"],\
+                display_constants.colours["on_yellow"],\
+                display_constants.colours["blink"],\
+                "Création de la base de données {}. Veuillez patienter !".format(db_constants.DB_NAME),\
+                display_constants.colours["default"],)
         except mysql.connector.Error as err:
             if err.errno == 1044:
                 print("Vous n'avez pas les droits d'accès à la base de donnée. Vérifier vos droit.")
 
             elif err.errno == errorcode.ER_BAD_DB_ERROR:
                 print("La base de données {} n'existe pas.".format(db_constants.DB_NAME))
-                print("Création de la base de données....")    
+                print("Création de la base de données {}".format(db_constants.DB_NAME))
                 self.cursor.execute(db_constants.CREATE_DB)
-                print("Base de données crée")
+                print("Base de données à été crée")
                 self.connect.database = db_constants.DB_NAME
 
     def create_tables(self):
         for table_query in db_constants.TABLES:
             self.cursor.execute( db_constants.TABLES[table_query] )
 
-    def insert_data_in_table(self, table, collum_name, data_value):
-        pass
+    def insert_data_to_database(self, api_data):
+        for data in api_data:
+            list_key =[]
+            list_row_values=[]
 
+            for api_key in constants.fr_food_informations.values():
+                if api_key in data.keys():
+                    if api_key == "categories":
+                        list_cat = data[api_key].split(",")
+                    elif api_key == "stores":
+                        list_stores = data[api_key].split(",")
+                    else:
+                        list_row_values.append(data[api_key])
+
+            # Insert product in table
+            self.cursor.execute(db_data_constants.INSERT_IN_PRODUCTS,list_row_values)
+            last_product_row_id = self.cursor.lastrowid
+
+            # Insert categories in table
+            for category in list_cat:
+                category = category.strip()
+                category = category.capitalize()
+                self.cursor.execute(db_data_constants. INSERT_IN_CATEGORIES, {"cat" : category})
+                # Insert the id's in linking table
+                self.cursor.execute(db_data_constants.INSERT_IN_PROD_CAT, {"id_prod" :last_product_row_id , "cat" : category })
+
+            # Insert stores in table
+            for store in list_stores:
+                store = store.strip()
+                store = store.upper()
+                self.cursor.execute(db_data_constants.INSERT_IN_STORES, {"store" : store })
+                # Insert the id's in linking table
+                self.cursor.execute(db_data_constants.INSERT_IN_PROD_STORE, {"id_prod" :last_product_row_id, "store" : store })
+
+            self.connect.commit()
 
     def close_connection(self):
         self.connect.close()
