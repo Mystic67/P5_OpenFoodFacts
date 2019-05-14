b00566f832a72a1fc944d0f47b8c23da92b77dab
Mystic67 <webmaster@passion-geek.com>
2019-05-14 16:05:04 +0200

    added menu text

--- config/display_settings.py
+++ config/display_settings.py
@@ -2,23 +2,59 @@
 # -*-coding: utf-8-*-
 
 
+main_choice_text = [ "\tMettre à jour la base de données.\t", "\tChercher un produit de substitution\t", "\tVoir mes aliments favoris enregistrés\t"
+
+]
+
 # Text messages
 messages = {
-"Welcome" : "                                                           Bienvenu dans l'application Open Food Alernative !                                                  ",
-"Category_choice" : "Veuillez saisir un numéro de categorie selon le choix proposé ('Q' ou 'q' pour quitter): ",
-"Product_choice" : "Veuillez saisir le numéro d'index du produit que vous souhaiter substituer ('Q' ou 'q' pour quitter) : ",
-"Pagination_info" : "Vous pouvez utiliser les entrées +/- pour faire défiler les pages ! "
+    "Welcome" : "\t\t\t\t\t\t\t\tBienvenu dans l'application Open Foods Alternatives !\t\t\t\t\t\t\t\t\t\t",
+    "Main_choice" : "Veuillez saisir un numéro selon les choix proposés ('Q' ou 'q' pour quitter l'application): ",
+    "Category_choice" : "Veuillez saisir un numéro de categorie selon les choix proposés ('Q' ou 'q' pour revenir au menu principa): ",
+    "Product_choice" : "Veuillez saisir le numéro d'index de l'aliment que vous souhaiter substituer ('Q' ou 'q' pour revenir au menu principal) : ",
+    "Substitute_choice" : "Veuillez saisir le numéro d'index de l'aliment de substitition à comparer ('Q' ou 'q' pour revenir au menu principal) : ",
+    "Save_substitute" : "Souhaitez-vous enregistrer l'aliment et son substitut dans vos favoris ? ('Oui' pour enregistrer/ Q pour revenir au menu principal ) : ",
+    "Pagination_info" : "Vous pouvez utiliser les entrées +/- pour faire défiler les pages ! ",
+    "Confim_saved" : "Le produit N° {} et son substitut N° {} ont été enregistré dans vos favoris: ",
+    "Favorites" : "Veuillez saisir les entrées +/- ('Q' ou 'q' pour revenir au menu principal) : "
 
 }
+
+# Text Menus
+text_menus = {
+    "Main_menu" :          "            Menu principal            ",
+    "Category_choice" :    "          Choix de catégorie.         ",
+    "Food_choice" :        "    Choix de l'aliment à substituer   ",
+    "Substitute_choice" :  "  Choix d'un aliment de substitution  ",
+    "product_comparison" : "Comparatif des aliments/Enregistrement",
+    "products_saved" :     " Mes aliments de substitution favoris ",
+}
+
 # Text errors
 errors = {
-"ValueError" : " Votre entrée n'est pas valide, veuillez recommencer."
+    "ValueError" : " Cette entrée n'est pas valide ! Veuillez recommencer.",
+    "Save_error" : " L'enregistrement dans la base de données à échoué !"
 }
 
 counter = "Vous avez {} résultat(s) pour votre requête."
 
+separate_text = {
+    "Main_question" : "\tQue voulez-vous faire ?\t",
+    "Product" : "Aliment à substituer",
+    "Substitute" : "Aliment de substitution",
+
+}
+
+system_info = {
+    "Api_Search" : " Récupération des données d'OpenFoodFacts. Veuillez patienter !",
+    "Table_update" : " Mise à jour de la table '{}' de base de données. Veuillez patienter !",
+    "Update_completed" : " Mise à jour terminé ! "
+
+}
+
 # Variable to del the preview line (Error message )
-del_preview_line = "\033[A"
+del_line = "\033[2K"
+return_to_preview_line = "\033[A"
 
 # Colors choice for color the user interface
 colours = {
