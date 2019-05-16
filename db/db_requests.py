eabb62f8a4ef41a255f8e59d2a239e0a95327fe1
Mystic67 <webmaster@passion-geek.com>
2019-05-16 17:13:07 +0200

    add find substitutes method and refactoring

--- db/db_requests.py
+++ db/db_requests.py
@@ -9,57 +9,45 @@ class DB_requests:
     database, allow to flip through the result pages and save the substitutes  """
     def __init__(self, db_instance):
         self.db = db_instance
-        self.offset = 0
 
-    def find_products(self, query, id, category=None, limit=8):
-        self.db.cursor.execute(query, {"id": id, "category": category,
-                                       "limit": limit, "offset": self.offset})
+    def find_products(self, query, id, category=None):
+        self.db.cursor.execute(query, {"id": id, "category": category})
         result = self.db.cursor.fetchall()
+        return result
+
+    def find_substitutes(self, query, id, category = None):
+        startchar = 1
+        endchar = 6
+        while 1:
+            self.db.cursor.execute(queries_constants.FIND_GENERIC_WORD,{"id": id, "startchar" : startchar ,"endchar" : endchar})
+            word = self.db.cursor.fetchall()
+            word1, = word[0]
+            self.db.cursor.execute(query, {"id": id, "category": category, "word" : word1})
+            result = self.db.cursor.fetchall()
+            if len(result) >= 1 and len(result) <= 20 :
+                break
+            if endchar < 100:
+                startchar = startchar + 5
+                endchar = endchar + 5
+            else:
+                startchar = 0
+                endchar = 0
+        print("mot clé : " ,word1)
+        return result
+
+    def find_all_favorites(self):
+        self.db.cursor.execute(queries_constants.FIND_FAVORITE_BY_ID)
+        result = self.db.cursor.fetchall()
+        favorites = []
+        for prod_id, subst_id in result:
+            #print(prod_id, subst_id)
+            favorites.append([self.find_products(
+                queries_constants.FIND_PRODUCTS_BY_ID, prod_id, None), self.find_products(
+                    queries_constants.FIND_PRODUCTS_BY_ID, subst_id, None)])
+        return favorites
+
+    def save_substitute(self, product_id, substitute_id):
         self.db.cursor.execute(
-            query, {
-                "id": id, "category": category, "limit": 1000, "offset": 0})
-        max_counter = len(self.db.cursor.fetchall())
-        self.nb_of_results = len(result)
-        self.limit = limit
-        return (result, max_counter)
-
-    def get_max_counter_request(self):
-        return self.max_counter
-
-    def increment_page(self):
-        if self.nb_of_results == 0:
-            self.offset = self.offset
-        else:
-            self.offset = self.offset + self.limit
-        return self.offset
-
-    def decrement_page(self):
-        if self.offset <= 0:
-            self.offset = 0
-        else:
-            self.offset = self.offset - self.limit
-
-    def reset_offset(self):
-        self.offset = 0
-
-    def save_substitute(self, query, product_id, substitute_id):
-        self.db.cursor.execute(
-            query, {
+            queries_constants.SAVE_SUBSTITUTE, {
                 "id_prod": product_id, "id_subst": substitute_id})
         self.db.connect.commit()
-
-    def find_favorite(self, query, limit=1):
-        self.db.cursor.execute(query, {"limit": limit, "offset": self.offset})
-        mem_offset = self.offset
-        result = self.db.cursor.fetchall()
-        [(prod_id, subst_id)] = result
-        self.nb_of_results = len(result)
-        self.db.cursor.execute(query, {"limit": 1000, "offset": 0})
-        max_counter = len(self.db.cursor.fetchall())
-        self.reset_offset()
-        product, counter1 = self.find_products(
-            queries_constants.FIND_PRODUCTS_BY_ID, prod_id, None, 1)
-        substitute, counter2 = self.find_products(
-            queries_constants.FIND_PRODUCTS_BY_ID, subst_id, None, 1)
-        self.offset = mem_offset
-        return (product, substitute, max_counter)
